904eadd58c6fd5470e14d5d40a74aceafd5fbdad
Cory Forsyth <cory.forsyth@gmail.com>
2010-12-16 18:30:03 -0500

    Ok I think we're done. sending postcard js works goodish.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -17,8 +17,21 @@ $(document).ready( function() {
     if ( $("#recipient_name").hasClass("blur") ||
          $("#recipient_address").hasClass("blur") ) {
            alert("I need both your name and address!");
-      $("#postage_stamp").animate({left: "10px", top: "30px"});
+      $("#postage_stamp").animate({left: "30px", top: "40px"});
       return false;
     }
   });
 });
+
+
+function sendPostcard() {
+  $("#postage_stamp").fadeOut(); $("#hidden_postage_stamp").fadeIn();
+  setTimeout( function() { $("#cancel_postage_stamp").fadeIn(); }, 500);
+  setTimeout( function() {
+    $("#postcard").animate({left: "1000px"});
+  }, 1500);
+  setTimeout( function() {
+    $("#postcard").hide();
+    $("#thanks").fadeIn();
+  }, 2000);
+}
