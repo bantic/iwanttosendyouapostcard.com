fdbcbe61c294f622b3989c46877d235f93b282a5
Cory Forsyth <cory.forsyth@gmail.com>
2010-12-16 18:10:34 -0500

    Fix drag-submitting

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready( function() {
-  $('input[title!=""]').hint();  
+  $('input[title!=""]').hint();
   $('textarea[title!=""]').hint();
   $("#postage_stamp").draggable();
   
@@ -9,7 +9,16 @@ $(document).ready( function() {
   $("#stamp_target").droppable({
     hoverClass: "hover",
     drop: function(event, ui) {
-      alert("dropped");
+      $("#new_recipient").submit();
     }
   });
-})
+  
+  $("#new_recipient").submit( function() {
+    if ( $("#recipient_name").hasClass("blur") ||
+         $("#recipient_address").hasClass("blur") ) {
+           alert("I need both your name and address!");
+      $("#postage_stamp").animate({left: "10px", top: "30px"});
+      return false;
+    }
+  });
+});
