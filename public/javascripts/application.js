fd22807d6997c7adf019bac730eeb0e9c8f86d21
Cory Forsyth <cory.forsyth@gmail.com>
2010-12-16 15:43:45 -0500

    Add stamp, drag/drop, etc.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,5 +2,14 @@
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready( function() {
   $('input[title!=""]').hint();  
-  $('textarea[title!=""]').hint();  
+  $('textarea[title!=""]').hint();
+  $("#postage_stamp").draggable();
+  
+  $("#draggable").draggable();
+  $("#stamp_target").droppable({
+    hoverClass: "hover",
+    drop: function(event, ui) {
+      alert("dropped");
+    }
+  });
 })
