46d4b577121ebfc188af8df5ace3a4e5ac0bdf77
Cory Forsyth <cory.forsyth@gmail.com>
2010-12-17 16:29:20 -0500

    fix javascript issue

--- public/javascripts/jquery.hint.js
+++ public/javascripts/jquery.hint.js
@@ -15,7 +15,7 @@ $.fn.hint = function (blurClass) {
             $win = $(window);
 
         function remove() {
-            var value_replaced = this.value.replace(/\s/g,"");
+            var value_replaced = (this.value || "").replace(/\s/g,"");
             var title_replaced = title.replace(/\s/g,"");
             if (value_replaced === title_replaced && $input.hasClass(blurClass)) {
                 $input.val('').removeClass(blurClass);
